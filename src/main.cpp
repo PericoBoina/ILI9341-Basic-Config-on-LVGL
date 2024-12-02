0ff0d9d8128b8c31a784e04560c3f47077188325
FoxLab <foxlab97@gmail.com>
2024-12-02 20:34:15 +0100

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,9 @@
 
 TFT_eSPI tft = TFT_eSPI();     // Inicializa TFT_eSPI
 
-/*Set to your screen resolution and rotation*/
 #define TFT_ROTATION LV_DISPLAY_ROTATION_0
-
-/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
 #define DRAW_BUF_SIZE (TFT_HEIGHT * TFT_WIDTH / 10 * (LV_COLOR_DEPTH / 8))
 uint32_t draw_buf[DRAW_BUF_SIZE / 4];
-
-
-/*use Arduinos millis() as tick source*/
 static uint32_t my_tick(void)
 {
     return millis();
@@ -25,15 +19,12 @@ void setup()
     Serial.begin(115200);
     Serial.println(LVGL_Arduino);
     lv_init();
-    /*Set a tick source so that LVGL will know how much time elapsed. */
     lv_tick_set_cb(my_tick);
     lv_display_t *disp;
-    /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
     disp = lv_tft_espi_create(TFT_WIDTH, TFT_HEIGHT, draw_buf, sizeof(draw_buf));
     lv_obj_t *label = lv_label_create(lv_screen_active());
     lv_label_set_text(label, "Hello @Perico_197, I'm LVGL!");
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
-
     Serial.println("Setup done");
 }
 
