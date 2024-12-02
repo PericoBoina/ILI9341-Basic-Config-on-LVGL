8083f2f7edea21918dc16d7983488f4b6f51fcee
FoxLab <foxlab97@gmail.com>
2024-12-02 22:08:07 +0100

    deleted:    include/README 	deleted:    include/User_Setup.h 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,59 @@
 #include <Arduino.h>
-#include <lvgl.h>     // Biblioteca LVGL
+#include <lvgl.h>
 #include <TFT_eSPI.h>
 
-TFT_eSPI tft = TFT_eSPI();     // Inicializa TFT_eSPI
+TFT_eSPI tft = TFT_eSPI();
 
 #define TFT_ROTATION LV_DISPLAY_ROTATION_0
 #define DRAW_BUF_SIZE (TFT_HEIGHT * TFT_WIDTH / 10 * (LV_COLOR_DEPTH / 8))
+
 uint32_t draw_buf[DRAW_BUF_SIZE / 4];
+
 static uint32_t my_tick(void)
 {
     return millis();
 }
 
+lv_obj_t *bar1;
+lv_obj_t *bar2;
+
+static void set_temp(void *bar, int32_t temp)
+{
+    lv_bar_set_value((lv_obj_t *)bar, temp, LV_ANIM_ON);
+}
+
 void setup()
 {
-    String LVGL_Arduino = "Hello @Perico_197! ";
-    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
-    Serial.begin(115200);
-    Serial.println(LVGL_Arduino);
     lv_init();
     lv_tick_set_cb(my_tick);
     lv_display_t *disp;
     disp = lv_tft_espi_create(TFT_WIDTH, TFT_HEIGHT, draw_buf, sizeof(draw_buf));
+
     lv_obj_t *label = lv_label_create(lv_screen_active());
-    lv_label_set_text(label, "Hello @Perico_197, I'm LVGL!");
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
-    Serial.println("Setup done");
+    lv_label_set_text(label, "Hello @Perico_197 in LVGL");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, -140);
+
+    bar1 = lv_bar_create(lv_screen_active());
+    lv_obj_set_size(bar1, 150, 20);
+    lv_obj_align(bar1, LV_ALIGN_CENTER, 0, -110);
+    lv_bar_set_value(bar1, 50, LV_ANIM_ON);
+
+    bar2 = lv_bar_create(lv_screen_active());
+    lv_obj_set_size(bar2, 150, 20);
+    lv_obj_align(bar2, LV_ALIGN_CENTER, 0, -90);
+    lv_bar_set_value(bar2, 60, LV_ANIM_ON);
+
+    lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_exec_cb(&a, set_temp);
+    lv_anim_set_duration(&a, 3000);
+    lv_anim_set_playback_duration(&a, 3000);
+    lv_anim_set_var(&a, bar2);
+    lv_anim_set_values(&a, 0 , 100);
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+    lv_anim_start(&a);
+
+
 }
 
 void loop()
